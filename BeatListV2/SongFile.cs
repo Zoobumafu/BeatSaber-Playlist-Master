61c4f059493808b3bd66a8ca0ecc1d8bdd1d50f6
TomerAtar <48738088+TomerAtar@users.noreply.github.com>
2021-03-31 14:21:14 +0300

    Added 1.0.0 code

---
+++ BeatListV2/SongFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatSaberUltimatePlaylistEditor
+{
+    class SongFile
+    {
+        public string _songName { set; get; }
+        public string _levelAuthorName { set; get; }
+        public string _songAuthorName { set; get; }
+        public string _beatsPerMinute { set; get; }
+        public string _coverImageFilename { set; get; }
+
+        public string filePath;
+
+        //public string fullSongName;
+
+        public DateTime lastModified;
+    }
+}
